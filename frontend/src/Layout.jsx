e2490b93579df8178b0a00902f53422bcb9e245c
sunil kumar <119299945+sunilp7023@users.noreply.github.com>
2023-04-06 12:01:08 +0530

    inital submit

---
+++ frontend/src/Layout.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+import {Outlet} from "react-router-dom";
+
+export default function Layout() {
+  return (
+    
+    <div className="py-4 px-8 flex flex-col min-h-screen  mx-auto">
+      <Header />
+      <Outlet />
+    </div>
+  );
+}
